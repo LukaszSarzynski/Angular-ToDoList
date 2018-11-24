7253830c3d74e5c76f42a7b688dc748b21b07a44
Lukasz Sarzynski <44730987+LukaszSarzynski@users.noreply.github.com>
2018-11-24 22:32:50 +0100

    added option to done task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,22 @@ export class AppComponent {
   bClickInputAdd: boolean;
   sNewTask: string;
   asTasks: Array<string> = [];
+  asTaskSuccessful: Array<string> = [];
 
   addNewTask() {
     if (this.sNewTask.length > 0) {
       this.asTasks.push(this.sNewTask);
     }
-    // console.log(this.asTasks);
     this.sNewTask = '';
   }
 
   removeTask(sTaskToRemove: string) {
     this.asTasks = this.asTasks.filter( sTask => sTask !== sTaskToRemove);
+  }
+
+  doneTask(sTask: string) {
+    this.removeTask(sTask);
+    this.asTaskSuccessful.push(sTask);
   }
 
 }
--- src/app/material.ts
+++ src/app/material.ts
@@ -5,9 +5,10 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import {MatListModule} from '@angular/material/list';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   exports: [MatButtonModule, MatCheckboxModule, MatIconModule, MatToolbarModule,
-    MatInputModule, MatListModule, MatTooltipModule]
+    MatInputModule, MatListModule, MatTooltipModule, MatGridListModule]
 })
 export class MaterialModule { }
